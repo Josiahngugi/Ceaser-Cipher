ab089163496b0ec33c2a74cdc13664ad15a04435
josiah <mainajosiahdev@gmail.com>
2018-09-28 12:39:55 -0400

    Comitting the choice on Encrypting or Decrypting

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,3 +1,10 @@
-public class Cipher  {
+import java.util.Scanner;
 
+public class Cipher  {
+public static void main(String[] args){
+    System.out.println("1.Encryption\n2.Decryption\nChoose(1,2):");
+    Scanner input = new Scanner(System.in);
+    int Choice = input.nextInt();
+
+}
 }
