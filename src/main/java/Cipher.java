d7f6df0c15ac94ae9b39aec7e5bf1e27d29f177d
josiah <mainajosiahdev@gmail.com>
2018-09-29 07:10:05 -0400

    Comitting the fuctionality part of the programs

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,5 +1,6 @@
 public class Cipher  {
 public static String encrypt(String message, int key){
+
     if(key>26){
         key=key%26;
     }
@@ -38,6 +39,55 @@ public static String encrypt(String message, int key){
         }
     }
     return ciphermessage;
+}
+    public static String dencrypt(String message, int key){
+
+        if(key>26){
+            key=key%26;
+        }
+        else if(key<0){
+            key =(key%26)+26;
+        }
+        String ciphermessage="";
+        int length=message.length();
+        for(int i=0;i<length;i++){
+            char ch=message.charAt(i);
+            if(Character.isLetter(ch)){
+                ciphermessage += ch;
+                if(Character.isLowerCase(ch)){
+                    char c=(char)(ch+=-key);
+                    if(c<'z'){
+                        ciphermessage +=(char)(ch+(26-key));
+                    }
+                    else {
+                        ciphermessage +=c;
+                    }
+                }
+                else if(Character.isLowerCase(ch)){
+                    char c=(char)(ch-key);
+                    if(c<'Z'){
+                        ciphermessage +=(char)(ch+(26-key));
+                    }
+                    else {
+                        ciphermessage +=c;
+                    }
+                }
+
+            }
+            else {
+                ciphermessage +=ch;
+
+            }
+        }
+        return ciphermessage;
+    }
+public static void main(String[] args){
+
+   String text ="maina";
+   String cipher=encrypt(text,4);
+   System.out.println(cipher);
+   String decrypt=dencrypt(cipher,4);
+   System.out.println(decrypt);
 }
 }
 
--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,5 +1,20 @@
-public class app {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class app {
+
+    public static void main(String[] args){
+        Scanner input =  new Scanner(System.in);
+        System.out.println("Welcome to Ceaser Cipher");
+        System.out.println(" choice\n 1.Encryption\n 2.Decryption");
+        int choice=input.nextInt();
+        if(choice==1){
+            System.out.println("Encrypting...");
+            Cipher encrypting= new Cipher();
+
+
+        }
+        else{
+            System.out.println("Decrypting..");
+        }
     }
 }
