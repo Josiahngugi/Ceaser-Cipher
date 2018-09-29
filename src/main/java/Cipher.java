c68a4eff0ca0b2788af1fa4b514b09193a85ac90
josiah <mainajosiahdev@gmail.com>
2018-09-29 04:31:36 -0400

    Comitting the initial refactor

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,6 +1,45 @@
 public class Cipher  {
+public static String encrypt(String message, int key){
+    if(key>26){
+        key=key%26;
+    }
+    else if(key<0){
+        key =(key%26)+26;
+    }
+    String ciphermessage="";
+    int length=message.length();
+    for(int i=0;i<length;i++){
+        char ch=message.charAt(i);
+        if(Character.isLetter(ch)){
+            ciphermessage += ch;
+            if(Character.isLowerCase(ch)){
+                char c=(char)(ch+key);
+                if(c>'z'){
+                    ciphermessage +=(char)(ch-(26-key));
+                }
+                else {
+                    ciphermessage +=c;
+                }
+            }
+            else if(Character.isLowerCase(ch)){
+                char c=(char)(ch+key);
+                if(c>'Z'){
+                    ciphermessage +=(char)(ch-(26-key));
+                }
+                else {
+                    ciphermessage +=c;
+                }
+            }
 
-           }
+        }
+        else {
+            ciphermessage +=ch;
+
+        }
+    }
+    return ciphermessage;
+}
+}
 
 
 
--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,4 +1,5 @@
 public class app {
     public static void main(String[] args){
+
     }
 }
