03accbd184eda5d5dd3be36d86a4aa9fee6b686b
josiah <mainajosiahdev@gmail.com>
2018-10-08 11:24:52 +0300

    Refactoring the encrypt method

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -11,10 +11,8 @@ public class Cipher  {
         String cipherText = "";
 
         for (int i = 0; i < plainText.length(); i++) {
-            int charPosition = ALPHABET.indexOf(plainText.charAt(i));
-            int keyVal = (shiftKey + charPosition) % 26;
-            char replaceVal = ALPHABET.charAt(keyVal);
-            cipherText += replaceVal;
+            int keyVal = (shiftKey + ALPHABET.indexOf(plainText.charAt(i))) % 26;
+            cipherText += ALPHABET.charAt(keyVal);
         }
         return cipherText;
 
