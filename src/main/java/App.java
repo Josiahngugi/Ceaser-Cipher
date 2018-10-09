80e757dbb740fd51fc12e95e23883e61cd8783f4
josiah <mainajosiahdev@gmail.com>
2018-10-09 07:27:32 +0300

    Commiting on user Interaction feature

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,29 +5,32 @@ import java.util.Scanner;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to Code Cipher!");
-        System.out.println("choice\n 1.Encrypt\n 2.Decrypt");
-        int choice=sc.nextInt();
-        Cipher caeser=new Cipher();
-        if(choice ==1){
-            System.out.println("Enter the String for Encryption: ");
-            String message = new String();
-            message = sc.next();
-            System.out.println("Enter Key");
-            int shiftKey=sc.nextInt();
-            System.out.println(caeser.encrypt(message,shiftKey));
+        for( ;; ) {
+            System.out.println("choice\n 1.Encrypt\n 2.Decrypt\n3.Exit");
+            int choice = sc.nextInt();
+            Cipher caeser = new Cipher();
+            if (choice == 1) {
+                System.out.println("Enter the String for Encryption: ");
+                String message = new String();
+                message = sc.next();
+                System.out.println("Enter Key");
+                int shiftKey = sc.nextInt();
+                System.out.println(caeser.encrypt(message, shiftKey));
 
-        }
-        else if(choice==2){
-            System.out.println("Enter the String for Decryption: ");
-            String message = new String();
-            message = sc.next();
-            System.out.println("Enter Key");
-            int shiftKey=sc.nextInt();
-            System.out.println(caeser.decrypt(message, 3));
+            } else if (choice == 2) {
+                System.out.println("Enter the String for Decryption: ");
+                String message = new String();
+                message = sc.next();
+                System.out.println("Enter Key");
+                int shiftKey = sc.nextInt();
+                System.out.println(caeser.decrypt(message, 3));
 
-        }
-        else {
-            System.out.println("Invalid choice");
+            } else if (choice == 3) {
+                System.exit(0);
+
+            } else {
+                System.out.println("Invalid choice");
+            }
         }
 
 
--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,4 +1,4 @@
-public class Cipher  {
+public class Cipher {
     private static String plainText;
     private int shiftKey;
 
@@ -7,93 +7,84 @@ public class Cipher  {
         plainText = getplainText;
         String cipherText = "";
 
-        if(shiftKey>26){
-            shiftKey=(shiftKey%26);
-        }
-        else if(shiftKey<0){
-            shiftKey=(shiftKey%26)+26;
+        if (shiftKey > 26) {
+            shiftKey = (shiftKey % 26);
+        } else if (shiftKey < 0) {
+            shiftKey = (shiftKey % 26) + 26;
         }
 
 
         for (int i = 0; i < plainText.length(); i++) {
-            if(Character.isLetter(plainText.charAt(i))){
-                if(Character.isLowerCase(plainText.charAt(i))){
-                    char ciphered_letter=(char)(plainText.charAt(i)+shiftKey);
-                    if(ciphered_letter>'z'){
-                        cipherText+=(char)(plainText.charAt(i)-(26-shiftKey));
+            if (Character.isLetter(plainText.charAt(i))) {
+                if (Character.isLowerCase(plainText.charAt(i))) {
+                    char ciphered_letter = (char) (plainText.charAt(i) + shiftKey);
+                    if (ciphered_letter > 'z') {
+                        cipherText += (char) (plainText.charAt(i) - (26 - shiftKey));
 
+                    } else {
+                        cipherText += ciphered_letter;
                     }
-                    else {
-                        cipherText+=ciphered_letter;
-                    }
-                }
-                else if(Character.isUpperCase(plainText.charAt(i))){
-                    char ciphered_letter=(char)(plainText.charAt(i)+shiftKey);
-                    if(ciphered_letter>'Z'){
-                        cipherText+=(char)(plainText.charAt(i)-(26-shiftKey));;
-                    }
-
-                    else {
-                        cipherText+=ciphered_letter;
+                } else if (Character.isUpperCase(plainText.charAt(i))) {
+                    char ciphered_letter = (char) (plainText.charAt(i) + shiftKey);
+                    if (ciphered_letter > 'Z') {
+                        cipherText += (char) (plainText.charAt(i) - (26 - shiftKey));
+                        ;
+                    } else {
+                        cipherText += ciphered_letter;
                     }
                 }
 
-            }
-            else {
-                cipherText+=plainText.charAt(i);
+            } else {
+                cipherText += plainText.charAt(i);
             }
 
         }
         return cipherText;
 
     }
-    public static String decrypt(String getplainText, int shiftKey)
-    {
+
+    public static String decrypt(String getplainText, int shiftKey) {
         plainText = getplainText;
         String cipherText = "";
 
-        if(shiftKey>26){
-            shiftKey=(shiftKey%26);
+        if (shiftKey > 26) {
+            shiftKey = (shiftKey % 26);
+        } else if (shiftKey < 0) {
+            shiftKey = (shiftKey % 26) + 26;
         }
-        else if(shiftKey<0){
-            shiftKey=(shiftKey%26)+26;
-        }
+
 
         for (int i = 0; i < plainText.length(); i++) {
-            if(Character.isLetter(plainText.charAt(i))){
-                if(Character.isLowerCase(plainText.charAt(i))){
-                    char ciphered_letter=(char)(plainText.charAt(i)-shiftKey);
-                    if(ciphered_letter<'a'){
-                        cipherText+=(char)(plainText.charAt(i)+(26-shiftKey));;
+            if (Character.isLetter(plainText.charAt(i))) {
+                if (Character.isLowerCase(plainText.charAt(i))) {
+                    char ciphered_letter = (char) (plainText.charAt(i) - shiftKey);
+                    if (ciphered_letter < 'a') {
+                        cipherText += (char) (plainText.charAt(i) + (26 - shiftKey));
 
+                    } else {
+                        cipherText += ciphered_letter;
                     }
-                    else {
-                        cipherText+=ciphered_letter;
-                    }
-                }
-                else if(Character.isUpperCase(plainText.charAt(i))){
-                    char ciphered_letter=(char)(plainText.charAt(i)-shiftKey);
-                    if(ciphered_letter<'A'){
-                        cipherText+=(char)(plainText.charAt(i)+(26-shiftKey));;
-                    }
-
-                    else {
-                        cipherText+=ciphered_letter;
+                } else if (Character.isUpperCase(plainText.charAt(i))) {
+                    char ciphered_letter = (char) (plainText.charAt(i) - shiftKey);
+                    if (ciphered_letter < 'A') {
+                        cipherText += (char) (plainText.charAt(i) + (26 - shiftKey));
+                        ;
+                    } else {
+                        cipherText += ciphered_letter;
                     }
                 }
 
-            }
-            else {
-                cipherText+=plainText.charAt(i);
+            } else {
+                cipherText += plainText.charAt(i);
             }
 
         }
         return cipherText;
+
 
     }
 
 }
-
 
 
 
